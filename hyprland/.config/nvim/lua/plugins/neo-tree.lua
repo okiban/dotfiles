8b97aeefbb0c4d88d98e21e045bb5757726ed085
okiban <>
2025-06-21 16:25:26 +0200

    change neotree for yazi

--- hyprland/.config/nvim/lua/plugins/neo-tree.lua
+++ hyprland/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,29 +2,29 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal position=float<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  -- 'nvim-neo-tree/neo-tree.nvim',
+  -- version = '*',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'nvim-tree/nvim-web-devicons',
+  --   'MunifTanjim/nui.nvim',
+  -- },
+  -- lazy = false,
+  -- keys = {
+  --   { '\\', ':Neotree reveal position=float<CR>', desc = 'NeoTree reveal', silent = true },
+  -- },
+  -- opts = {
+  --   filesystem = {
+  --     filtered_items = {
+  --       visible = true,
+  --       hide_dotfiles = false,
+  --       hide_gitignored = true,
+  --     },
+  --     window = {
+  --       mappings = {
+  --         ['\\'] = 'close_window',
+  --       },
+  --     },
+  --   },
+  -- },
 }
