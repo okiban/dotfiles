695cbb78c328f26a058eed93b7766013c5e70310
okiban <>
2025-06-27 15:44:34 +0200

    add wallpaper + randomizer + hyprlock

--- hyprland/.config/hypr/change_wallpapers.sh
+++ hyprland/.config/hypr/change_wallpapers.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Wait until hyprpaper socket is available
+SOCKET_PATH=$(find /run/user/$(id -u)/hypr/ -name ".hyprpaper.sock" 2>/dev/null)
+while [ ! -S "$SOCKET_PATH" ]; do
+    sleep 0.5
+    SOCKET_PATH=$(find /run/user/$(id -u)/hypr/ -name ".hyprpaper.sock" 2>/dev/null)
+done
+
 WALLPAPER_DIR="$HOME/.config/hypr/wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
