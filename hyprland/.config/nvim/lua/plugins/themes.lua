b3ebe779c6fb7c6372a7a636e9af323e10cbd345
okiban <>
2025-06-29 19:24:01 +0200

    remove unused onedark themes

--- hyprland/.config/nvim/lua/plugins/themes.lua
+++ hyprland/.config/nvim/lua/plugins/themes.lua
@@ -1,15 +1,12 @@
 return {
-  'navarasu/onedark.nvim',
+  'olimorris/onedarkpro.nvim',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme = 'onedark'
+    vim.cmd.colorscheme = 'onedarkpro'
     vim.cmd.hi 'Comment gui=none'
   end,
   config = function()
-    require('onedark').setup {
-      style = 'darker',
-    }
-
-    require('onedark').load()
+    require('onedarkpro.config').set_theme 'onedark_dark'
+    require('onedarkpro').load()
   end,
 }
