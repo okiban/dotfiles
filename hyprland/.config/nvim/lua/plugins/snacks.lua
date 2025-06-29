cd04ab5c63acca02f4561006ed9982692eee84a5
okiban <>
2025-06-29 22:42:04 +0200

    add snacks configs

---
+++ hyprland/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,26 @@
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    image = {
+      enabled = true,
+      formats = {
+        'png',
+        'jpg',
+        'jpeg',
+        'gif',
+        'bmp',
+        'webp',
+        'tiff',
+        'heic',
+        'avif',
+        'mp4',
+        'mov',
+        'avi',
+        'mkv',
+        'webm',
+        'pdf',
+      },
+    },
+  },
+}
