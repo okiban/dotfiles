1b8894be794fd200b32823ff1a41753e7620b18a
okiban <>
2025-07-01 15:46:31 +0200

    add dashboard

--- hyprland/.config/nvim/lua/plugins/snacks.lua
+++ hyprland/.config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,33 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[
+██╗    ██╗   ██╗███████╗███████╗    ███╗   ██╗██╗   ██╗██╗███╗   ███╗    ██████╗ ████████╗██╗    ██╗
+██║    ██║   ██║██╔════╝██╔════╝    ████╗  ██║██║   ██║██║████╗ ████║    ██╔══██╗╚══██╔══╝██║    ██║
+██║    ██║   ██║███████╗█████╗      ██╔██╗ ██║██║   ██║██║██╔████╔██║    ██████╔╝   ██║   ██║ █╗ ██║
+██║    ██║   ██║╚════██║██╔══╝      ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║    ██╔══██╗   ██║   ██║███╗██║
+██║    ╚██████╔╝███████║███████╗    ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║    ██████╔╝   ██║   ╚███╔███╔╝
+╚═╝     ╚═════╝ ╚══════╝╚══════╝    ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝    ╚═════╝    ╚═╝    ╚══╝╚══╝ 
+]],
+       -- stylua: ignore
+       ---@type snacks.dashboard.Item[]
+       keys = {
+         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+         { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+         { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+       },
+      },
+    },
     image = {
       enabled = true,
       formats = {
