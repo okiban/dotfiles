4bede06eb75f40c399443d0a2528f6fffec25a80
okiban <>
2025-07-02 00:42:42 +0200

    decrease ms before showing docs

--- hyprland/.config/nvim/lua/plugins/autocompletion.lua
+++ hyprland/.config/nvim/lua/plugins/autocompletion.lua
@@ -49,7 +49,7 @@ return {
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 10,
+        auto_show_delay_ms = 1,
       },
     },
 
