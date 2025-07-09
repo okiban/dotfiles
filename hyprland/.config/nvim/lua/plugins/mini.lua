5dd434f180bfc580cfeb62f28b38843047374223
okiban <>
2025-07-09 23:23:16 +0200

    add status line muchos bangos

--- hyprland/.config/nvim/lua/plugins/mini.lua
+++ hyprland/.config/nvim/lua/plugins/mini.lua
@@ -12,14 +12,5 @@ return {
         comment_visual = '<C-q>',
       },
     }
-
-    local statusline = require 'mini.statusline'
-    statusline.setup {
-      use_icons = vim.g.have_nerd_font,
-    }
-
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
   end,
 }
