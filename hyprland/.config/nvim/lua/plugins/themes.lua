4759563c0261f3a4eedf6c41ad0420c68c083713
okiban <>
2025-07-14 01:22:41 +0200

    update

--- hyprland/.config/nvim/lua/plugins/themes.lua
+++ hyprland/.config/nvim/lua/plugins/themes.lua
@@ -1,10 +1,6 @@
 return {
   'olimorris/onedarkpro.nvim',
   priority = 1000,
-  init = function()
-    vim.cmd.colorscheme = 'onedarkpro'
-    vim.cmd.hi 'Comment gui=none'
-  end,
   config = function()
     require('onedarkpro.config').set_theme 'onedark_dark'
     require('onedarkpro').setup {
