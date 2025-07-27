5618fae66c18567eaf87d76be99d423d7338f52c
okiban <>
2025-07-28 00:39:38 +0200

    add trouble.nvim

--- hyprland/.config/nvim/lua/keymaps.lua
+++ hyprland/.config/nvim/lua/keymaps.lua
@@ -1,11 +1,6 @@
--- [[ Basic Keymaps ]]
+j- [[ Basic Keymaps ]]
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<C-C>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
-  desc = 'Open diagnostic [Q]uickfix list',
-})
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', {
   desc = 'Exit terminal mode',
